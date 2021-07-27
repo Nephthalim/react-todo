845abcd140063bf5eb8d94bbe94c42abfc1f59b5
Nephthalim Abebe <nephthalimabebe@Nephthalims-MacBook-Pro.local>
2021-07-26 23:27:42 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 const cors = require("cors");
 
 app.use(express.json());
@@ -9,7 +10,9 @@ const server_port = process.env.PORT || 5000;
 
 app.use('/auth', require('./routes/jwtAuth'));
 app.use('/', require('./routes/dashboard'));
+
 app.use(express.static(path.join(__dirname, "client/build")))
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/public/index.html'))
 })
