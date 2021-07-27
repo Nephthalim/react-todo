890d193479174eb62467df2c50cf3a5033674515
Nephthalim Abebe <nephthalimabebe@Nephthalims-MacBook-Pro.local>
2021-07-26 23:01:57 -0400

    Initial commit

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const server_port = process.env.PORT || 5000;
 app.use('/auth', require('./routes/jwtAuth'));
 app.use('/', require('./routes/dashboard'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/public/index.html'))
+})
+
 app.listen(server_port, () => {
     console.log(`Listening on port ${server_port}`)
 })
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const pool = require("../db");
 const authorization = require('../middleware/authorization')
 
+
 router.get('/', authorization, async(req, res) => {
     try {
         const user = await pool.query("SELECT * FROM users WHERE id=$1", [req.user]);
