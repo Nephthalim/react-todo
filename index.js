a380a85d7181210552b00f39e9a98f80207c5844
Nephthalim Abebe <nephthalimabebe@Nephthalims-MacBook-Pro.local>
2021-07-26 22:56:00 -0400

    Initial commit

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-const server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
-const server_host = process.env.YOUR_HOST || '127.0.0.1';
+const server_port = process.env.PORT || 5000;
 
 app.use('/auth', require('./routes/jwtAuth'));
 app.use('/', require('./routes/dashboard'));
 
-app.listen(server_port, server_host, () => {
+app.listen(server_port, () => {
     console.log(`Listening on port ${server_port}`)
 })
