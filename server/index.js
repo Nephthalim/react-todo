24faed8ed1ba951a2c2244c66862a3130f465401
Nephthalim Abebe <nephthalimabebe@Nephthalims-MacBook-Pro.local>
2021-07-27 19:00:53 -0400

    Deployment

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const cors = require("cors");
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 app.use(express.json());
 app.use(cors());
@@ -11,13 +14,16 @@ const server_port = process.env.PORT || 5000;
 app.use('/auth', require('./routes/jwtAuth'));
 app.use('/', require('./routes/dashboard'));
 
-app.use(express.static(path.join(__dirname, "client/build")))
+// view engine setup
 
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/public/index.html'))
-})
 
-app.listen(server_port, () => {
-    console.log(`Listening on port ${server_port}`)
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+
+
+module.exports = app;
