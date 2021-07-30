620bd20307cdaefa00fc7d4157aa1e1052c79aa5
Nephthalim Abebe <nephthalimabebe@Nephthalims-MacBook-Pro.local>
2021-07-30 17:23:21 -0400

    Heroku

--- server/app.js
+++ server/app.js
@@ -6,14 +6,10 @@ var logger = require('morgan');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, 'build')));
--- server/index.js
+++ server/index.js
@@ -11,18 +11,16 @@ app.use(cors());
 
 const server_port = process.env.PORT || 5000;
 
-app.use('/auth', require('./routes/jwtAuth'));
-app.use('/', require('./routes/dashboard'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // view engine setup
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.use('/auth', require('./routes/jwtAuth'));
+app.use('/', require('./routes/dashboard'));
 
-
-app.use(logger('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-
-
-
-module.exports = app;
+app.listen(server_port, () => {
+    console.log(`Listening on port:${server_port}`);
+});
